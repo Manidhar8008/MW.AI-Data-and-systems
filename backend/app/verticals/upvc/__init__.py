5eab0a3dc63bf7b77a1ac5a33c171b3bac95717a
Manidhar Pati <manidharmanidhar2@gmail.com>
2026-09-11 17:07:03 +0530

    feat: export uPVC estimation pipeline

--- backend/app/verticals/upvc/__init__.py
+++ backend/app/verticals/upvc/__init__.py
@@ -1,12 +1,20 @@
 from .engineering import EngineeringResult, UPVCEngineeringAgent
+from .estimation import BOM, BOMLineItem, PriceBook, Quote, QuoteLineItem, UPVCEstimator, UPVCProductionPipeline
 from .models import GlassType, Measurement, Opening, OpeningType, UPVCConfiguration
 
 __all__ = [
+    "BOM",
+    "BOMLineItem",
     "EngineeringResult",
     "GlassType",
     "Measurement",
     "Opening",
     "OpeningType",
-    "UPVCConfiguration",
+    "PriceBook",
+    "Quote",
+    "QuoteLineItem",
     "UPVCEngineeringAgent",
+    "UPVCConfiguration",
+    "UPVCEstimator",
+    "UPVCProductionPipeline",
 ]
