69e87d4e2fe27b9f3b9a87b649dbe4f56f378e1d
Manidhar Pati <manidharmanidhar2@gmail.com>
2026-09-11 17:05:03 +0530

    feat: expose uPVC domain primitives

---
+++ backend/app/verticals/upvc/__init__.py
@@ -0,0 +1,12 @@
+from .engineering import EngineeringResult, UPVCEngineeringAgent
+from .models import GlassType, Measurement, Opening, OpeningType, UPVCConfiguration
+
+__all__ = [
+    "EngineeringResult",
+    "GlassType",
+    "Measurement",
+    "Opening",
+    "OpeningType",
+    "UPVCConfiguration",
+    "UPVCEngineeringAgent",
+]
