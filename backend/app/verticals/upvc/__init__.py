555a77bb75986d110ef2610b661ba9c53e7b760e
Manidhar Pati <manidharmanidhar2@gmail.com>
2026-09-11 17:08:18 +0530

    feat: expose uPVC fabrication pipeline

--- backend/app/verticals/upvc/__init__.py
+++ backend/app/verticals/upvc/__init__.py
@@ -1,20 +1,48 @@
 from .engineering import EngineeringResult, UPVCEngineeringAgent
-from .estimation import BOM, BOMLineItem, PriceBook, Quote, QuoteLineItem, UPVCEstimator, UPVCProductionPipeline
+from .estimation import BOM, BOMLineItem, PriceBook, Quote, QuoteLineItem, UPVCEstimator
+from .fabrication import (
+    BOMCategory,
+    CutMaterial,
+    CutPiece,
+    CutPlan,
+    FabricationLineItem,
+    FabricationRules,
+    GlassPiece,
+    HardwareItem,
+    ProductionOrder,
+    ProfileRule,
+    StockCut,
+    UPVCFabricationPlanner,
+)
 from .models import GlassType, Measurement, Opening, OpeningType, UPVCConfiguration
+from .pipeline import UPVCProductionPipeline, UPVCProductionResult
 
 __all__ = [
     "BOM",
+    "BOMCategory",
     "BOMLineItem",
+    "CutMaterial",
+    "CutPiece",
+    "CutPlan",
     "EngineeringResult",
+    "FabricationLineItem",
+    "FabricationRules",
+    "GlassPiece",
     "GlassType",
+    "HardwareItem",
     "Measurement",
     "Opening",
     "OpeningType",
     "PriceBook",
+    "ProductionOrder",
+    "ProfileRule",
     "Quote",
     "QuoteLineItem",
-    "UPVCEngineeringAgent",
+    "StockCut",
     "UPVCConfiguration",
+    "UPVCEngineeringAgent",
     "UPVCEstimator",
+    "UPVCFabricationPlanner",
     "UPVCProductionPipeline",
+    "UPVCProductionResult",
 ]
